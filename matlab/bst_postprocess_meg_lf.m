4a0e227e62160849dbbe1c5ca24d9e973e59497b
Takfarinas Medani <takfarinesmedani@live.fr>
2020-02-06 19:20:21 -0800

    updates MEG lf computation

--- matlab/bst_postprocess_meg_lf.m
+++ matlab/bst_postprocess_meg_lf.m
@@ -1,29 +1,31 @@
 function cfg = bst_postprocess_meg_lf(cfg)
-%% Compute the total magnetic field B = Bp + Bs
-% a- Compute the MEG Primary Magnetic field  % apply formula of Sarvas
+%% Compute the total magnetic field 
 dipoles_pos_orie = [kron(cfg.sourceSpace,ones(3,1)), kron(ones(length(cfg.sourceSpace),1), eye(3))];
-[Bp] = compute_B_primary(cfg.coilsLoc, dipoles_pos_orie, cfg.coilsProjection);
-% b- Get the MEG Primary Magnetic field
-% [Bs] = cfg.fem_meg_lf;
-% c- The total magnetic field B = Bp + Bs;
-Bs =  cfg.fem_meg_lf;
-B = Bp + Bs;
 
-% d- Apply the weight :
+% a- Compute the MEG Primary Magnetic field  % apply formula of Sarvas
+%primary B-field
+[Bp] = compute_B_primary(cfg.coilsLoc, dipoles_pos_orie, cfg.coilsProjection);
+
+% b- The total magnetic field B = Bp + Bs;
+%  full B-field
+Bs =  cfg.fem_meg_lf;
+
+mu = 4*pi*1e-4; % check the value of the units maybe it needs to be mu = 4*pi*1e-7
+Bfull = (mu/(4*pi)) * (Bp - Bs);
+
+% c- Apply the weight :
 if cfg.runFromBst == 1
     [channelIndex] = unique(cfg.MegChannel(:,1));
     nbChannel = length(channelIndex);
-    wighted_B =zeros(nbChannel,size(B,2));
+    wighted_B =zeros(nbChannel,size(Bfull,2));
     for iCh = 1 : nbChannel
         communChannel = find(iCh==cfg.MegChannel);
-        BcommunChannel = B(communChannel(:),:);
-        WcommunChannel =  cfg.MegChannel(communChannel(:), 8: end);%(communChannel',:);
+        BcommunChannel = Bfull(communChannel(:),:);
+        WcommunChannel =  cfg.MegChannel(communChannel(:), 8: end);
         wighted_B(iCh,:) = sum (BcommunChannel.*WcommunChannel,1);
-    end
-    
+    end    
     cfg.fem_meg_lf = wighted_B;
 end
-% cfg = rmfield(cfg,'lf_fem');
 end
 
 
