0d7b04d64304155221803695886ab5bf380fc970
Takfarinas Medani <takfarinesmedani@live.fr>
2020-03-10 19:55:32 -0700

    add new fonctions

---
+++ matlab/bstMri2ftMri.m
@@ -0,0 +1,20 @@
+function ftMri = bstMri2ftMri(bstMri, vox2ras)
+% Convert the brainstorm MRI file to Filedtrip MRI file (compatibel with ft_read_mri)
+% ftMri = bstMri2ftMri(bstMri) 
+% ftMri = bstMri2ftMri(bstMri, vox2ras) 
+% input bstMri : MRI struvture in the brainstorm format
+%         vox2ras : transfomation 4x4 matrix 
+% output  ftMri : MRI struvture in the fieltrip format
+
+% Takfarinas MEDANI, 03/10/2020
+
+if nargin == 1
+    vox2ras = [];
+end
+% Re-write to the fieldtrip format
+ftMri.dim = size(bstMri.Cube);
+ftMri.anatomy = bstMri.Cube;
+ftMri.hdr = bstMri.Header;
+ftMri.transform = vox2ras;
+ftMri.unit = 'mm'; % we assume that milimiter are used
+end
